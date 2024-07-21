e99329ad636ac36fa9835ee86e0c5ed6c0bbc4e5
Ry Ferguson <rytf12@gmail.com>
2024-07-22 00:51:13 +0100

    remove all existing css and html and show a static table

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+export default function FluffyTable() {
+    return (
+        <table>
+            <tr>
+                <th>Col 1</th>
+                <th>Col 2</th>
+                <th>Col 3</th>
+            </tr>
+            <tr>
+                <td>val1</td>
+                <td>va12</td>
+                <td>val3</td>
+            </tr>
+        </table>
+    )
+}
